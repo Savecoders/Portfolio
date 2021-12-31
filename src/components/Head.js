45c6401739f9d9ccec3b3c0fb8e6024e85488e37
Savecoders <pablopincay2003@gmail.com>
2021-12-31 12:53:39 -0500

    add bg-svg

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,31 +1,34 @@
 import NavBar from './Navbar';
+import background from '../Ellipse.svg';
 const Head = () => {
 	return (
 		<header className='bg-bg w-screen h-screen'>
 			<NavBar />
-			<section
-				className='mt-10 mx-auto max-w-screen-2xl px-8 
-											sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-24'
-			>
-				<h4 className='font-Lato py-6 text-blue text-3xl'>Hi, I'm</h4>
-				<h2
-					className='font-Lato font-semibold text-color drop-shadow-small-lg 
-											text-7xl py-5'
+			<div className='flex justify-center '>
+				<section
+					className='mt-10 mx-auto max-w-screen-2xl px-8 
+				sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-24'
 				>
-					Pablo Pincay
-				</h2>
-				<h3 className='font-Lato py-6 text-blue text-4xl'>Frontend Developer</h3>
+					<h4 className='font-Lato py-6 text-blue text-3xl'>Hi, I'm</h4>
+					<h2
+						className='font-Lato font-semibold text-color drop-shadow-small-lg 
+					text-7xl py-5'
+					>
+						Pablo Pincay
+					</h2>
+					<h3 className='font-Lato py-6 text-blue text-4xl'>Frontend Developer</h3>
 
-				<button className='bg-cyan text-color-secundary w-40 h-12 text-xl font-Lato rounded-md mt-5'>
-					Download CV
-				</button>
-			</section>
-			<div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
-				<img
-					className='h-56 w-full sm:h-72 md:h-96 lg:w-full lg:h-full opacity-20'
-					src='https://cdn.devdojo.com/tails/images/gradient-blob.svg'
-					alt=''
-				/>
+					<button className='bg-cyan text-color-secundary w-40 h-12 text-xl font-Lato rounded-md mt-5'>
+						Download CV
+					</button>
+				</section>
+				<section className='m-auto p-8 w-2/5 h-2/4'>
+					<img
+						className='rounded-3xl absoute right-0 z-0 opacity-75'
+						src={background}
+						alt='background'
+					/>
+				</section>
 			</div>
 		</header>
 	);
